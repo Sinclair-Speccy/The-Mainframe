ce5eb9b4b4e3505929715ef93a117ed062bab22c
Sinclair-Speccy <tashamckie55@gmail.com>
2023-03-25 18:02:35 +1100

    Stuff :P

---
+++ Dropdowns.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+/* When the user clicks on the button, 
+closes every dropdowns and open the only one passed as argument */
+
+/* Javascript only */
+function myFunction(element) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  
+  // element.nextSibling is the carriage return… The dropdown menu is the next next.
+  var thisDropdown = element.nextSibling.nextSibling;
+  
+  if (!thisDropdown.classList.contains('show')) {  // Added to hide dropdown if clicking on the one already open
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove('show');
+    }
+  }
+  
+  // Toggle the dropdown on the element clicked
+  thisDropdown.classList.toggle("show");
+}
+
+/* W3Schools function to close the dropdown when clicked outside. */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
