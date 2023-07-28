77d2681284d86036e6e817f34e41063a3e77d916
Sinclair-Speccy <tashamckie55@gmail.com>
2023-07-28 20:15:09 +1000

    Damn js

--- Off-Limits/Bypass-The-Filter/script.js
+++ Off-Limits/Bypass-The-Filter/script.js
@@ -4,11 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const contentArea = document.getElementById('contentArea');
     const offLimitsBox = document.getElementById('offLimitsBox');
     const consentKey = 'userConsent';
+    const previousPageKey = 'previousPage';
 
     // Check if the user has already given consent
     const userConsent = localStorage.getItem(consentKey);
     if (userConsent === 'true') {
-        redirectToBypassPage();
+        const previousPage = localStorage.getItem(previousPageKey);
+        if (previousPage) {
+            window.location.href = previousPage;
+        } else {
+            showBypassContent();
+        }
+    } else {
+        showConsentBox();
     }
 
     agreementCheckbox.addEventListener('change', function () {
@@ -16,15 +24,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     enterButton.addEventListener('click', function () {
-        localStorage.setItem(consentKey, 'true');
-        redirectToBypassPage();
+        if (agreementCheckbox.checked) {
+            localStorage.setItem(consentKey, 'true');
+            const previousPage = localStorage.getItem(previousPageKey);
+            if (previousPage) {
+                window.location.href = previousPage;
+            } else {
+                showBypassContent();
+            }
+        }
     });
 
-    function redirectToBypassPage() {
+    function showConsentBox() {
+        offLimitsBox.style.display = 'block';
+        contentArea.style.display = 'none';
+        enterButton.addEventListener('click', function () {
+            localStorage.setItem(previousPageKey, window.location.href);
+            window.location.href = 'https://sinclair-speccy.github.io/The-Mainframe/Off-Limits/index-special.html';
+        });
+    }
+
+    function showBypassContent() {
         offLimitsBox.style.display = 'none';
         contentArea.style.display = 'block';
-        setTimeout(() => {
-            window.location.href = 'Bypass-The-Filter/index.html';
-        }, 1000); // Redirect to the new page after 1 second delay
     }
 });
--- Off-Limits/script.js
+++ Off-Limits/script.js
@@ -8,7 +8,9 @@ document.addEventListener('DOMContentLoaded', function () {
     // Check if the user has already given consent
     const userConsent = localStorage.getItem(consentKey);
     if (userConsent === 'true') {
-        redirectToBypassPage();
+        showBypassContent();
+    } else {
+        showConsentBox();
     }
 
     agreementCheckbox.addEventListener('change', function () {
@@ -16,15 +18,100 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     enterButton.addEventListener('click', function () {
-        localStorage.setItem(consentKey, 'true');
-        redirectToBypassPage();
+        if (agreementCheckbox.checked) {
+            localStorage.setItem(consentKey, 'true');
+            showBypassContent();
+        }
     });
 
-    function redirectToBypassPage() {
+    function showConsentBox() {
+        offLimitsBox.style.display = 'block';
+        contentArea.style.display = 'none';
+        enterButton.addEventListener('click', function () {
+            window.location.href = 'https://sinclair-speccy.github.io/The-Mainframe/Off-Limits/Bypass-The-Filter/index.html';
+        });
+    }
+
+    function showBypassContent() {
         offLimitsBox.style.display = 'none';
         contentArea.style.display = 'block';
-        setTimeout(() => {
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const agreementCheckbox = document.getElementById('agreement');
+    const enterButton = document.getElementById('enterBtn');
+    const contentArea = document.getElementById('contentArea');
+    const offLimitsBox = document.getElementById('offLimitsBox');
+    const consentKey = 'userConsent';
+
+    // Check if the user has already given consent
+    const userConsent = localStorage.getItem(consentKey);
+    if (userConsent === 'true') {
+        showBypassContent();
+    } else {
+        showConsentBox();
+    }
+
+    agreementCheckbox.addEventListener('change', function () {
+        enterButton.disabled = !agreementCheckbox.checked;
+    });
+
+    enterButton.addEventListener('click', function () {
+        if (agreementCheckbox.checked) {
+            localStorage.setItem(consentKey, 'true');
+            showBypassContent();
+        }
+    });
+
+    function showConsentBox() {
+        offLimitsBox.style.display = 'block';
+        contentArea.style.display = 'none';
+        enterButton.addEventListener('click', function () {
             window.location.href = 'https://sinclair-speccy.github.io/The-Mainframe/Off-Limits/Bypass-The-Filter/index.html';
-        }, 1000); // Redirect to the new page after 1 second delay
+        });
+    }
+
+    function showBypassContent() {
+        offLimitsBox.style.display = 'none';
+        contentArea.style.display = 'block';
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const agreementCheckbox = document.getElementById('agreement');
+    const enterButton = document.getElementById('enterBtn');
+    const contentArea = document.getElementById('contentArea');
+    const offLimitsBox = document.getElementById('offLimitsBox');
+    const consentKey = 'userConsent';
+
+    // Check if the user has already given consent
+    const userConsent = localStorage.getItem(consentKey);
+    if (userConsent === 'true') {
+        showBypassContent();
+    } else {
+        showConsentBox();
+    }
+
+    agreementCheckbox.addEventListener('change', function () {
+        enterButton.disabled = !agreementCheckbox.checked;
+    });
+
+    enterButton.addEventListener('click', function () {
+        if (agreementCheckbox.checked) {
+            localStorage.setItem(consentKey, 'true');
+            showBypassContent();
+        }
+    });
+
+    function showConsentBox() {
+        offLimitsBox.style.display = 'block';
+        contentArea.style.display = 'none';
+        enterButton.addEventListener('click', function () {
+            window.location.href = 'https://sinclair-speccy.github.io/The-Mainframe/Off-Limits/Bypass-The-Filter/index.html';
+        });
+    }
+
+    function showBypassContent() {
+        offLimitsBox.style.display = 'none';
+        contentArea.style.display = 'block';
     }
 });
