ec480f82df30da6c3da3b3f342cb7be06ce834ed
Sinclair-Speccy <tashamckie55@gmail.com>
2023-11-30 19:57:02 +1100

    More small updates before 2024

---
+++ script.js
@@ -0,0 +1,24 @@
+// Replace 'username' and 'repository' with your GitHub username and repository name
+const username = 'Sinclair-Speccy';
+const repository = 'The-Mainframe';
+
+// GitHub API endpoint for getting the latest commit
+const apiUrl = `https://api.github.com/repos/${username}/${repository}/commits?per_page=1`;
+
+// Function to format date nicely
+const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+// Fetch the latest commit from GitHub API
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const lastCommitDate = data[0].commit.author.date;
+        document.getElementById('lastUpdated').textContent = `Last updated: ${formatDate(lastCommitDate)}`;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        document.getElementById('lastUpdated').textContent = 'Error fetching data';
+    });
